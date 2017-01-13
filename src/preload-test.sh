2d8fbf872c15b76ab7ab52088e5109cc599e3df9
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-01-13 12:03:24 -0500

    Test script: allow custom prefix, print sucess

--- src/preload-test.sh
+++ src/preload-test.sh
@@ -1,8 +1,13 @@
-#!/bin/sh -x
+#!/bin/sh
+
+PRELOAD_PREFIX="."
+if [ ! -z "$1" ]; then
+    PRELOAD_PREFIX="$1/"
+fi
 
 msg="1234567890abcdefghijklmnopqrstuv"
 
-./deltafs-preload-test /tmp/pt.$$
+$1/deltafs-preload-test /tmp/pt.$$
 got=`cat /tmp/pt.$$`
 rm -f /tmp/pt.$$
 
@@ -14,14 +19,15 @@ fi
 #
 # XXX: this assumes a SunOS/linux-style ld.so (won't work on macosx)
 #
-got=`env LD_PRELOAD=./libdeltafs-preload.so PDLFS_Testin=1 \
-	./deltafs-preload-test /tmp/pdlfs/pt.$$`
+got=`env LD_PRELOAD=$1/libdeltafs-preload.so PDLFS_Testin=1 \
+	$1/deltafs-preload-test /tmp/pdlfs/pt.$$`
 got=`echo ${got} | sed -e 's/.*FCLOSE/FCLOSE/'`   # dump openmpi msg
 want="FCLOSE: /pt.$$ $msg 32"
 
 if [ "$got" != "$want" ]; then
-    echo PDLFS MISMATCH... - $got
+    echo "Preload test failed (error $got)"
     exit 1
 fi
 
+echo "Preload test successful"
 exit 0
