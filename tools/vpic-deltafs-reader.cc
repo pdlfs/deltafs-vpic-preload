8f8074f3569a7bbb4b47044b294b5132de04124d
George Amvrosiadis <gamvrosi@cs.cmu.edu>
2017-02-26 18:04:07 -0500

    Clean up vpic reader code

--- tools/vpic-deltafs-reader.cc
+++ tools/vpic-deltafs-reader.cc
@@ -13,18 +13,15 @@
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
-#include <dirent.h>
 #include <fcntl.h>
 
 #include <deltafs/deltafs_api.h>
 #include <pdlfs-common/xxhash.h>
 #include <ch-placement.h>
 #include <map>
-#include <list>
 
 static const char* prefixes[] = { "electron_tracer", "ion_tracer", NULL };
 typedef std::map<int,FILE*> FileMap;
-typedef std::list<int> EpochList;
 
 static const char* argv0 = NULL;
 static struct ch_placement_instance* ch_inst = NULL;
@@ -38,7 +35,6 @@ static void usage(int ret)
     assert(argv0 != NULL);
     printf("\nusage: %s [options] -i input_dir -o output_dir\n\n"
            "  options:\n"
-           "    -d        Run in deltafs mode\n"
            "    -s size   Consistent-hash ring size\n"
            "    -v factor Consistent-hash virtual factor\n"
            "    -n num    Number of particles to read (from 1 to num)\n"
@@ -143,101 +139,17 @@ int deltafs_read_particles(long long int num, char *indir, char *outdir)
     return 0;
 }
 
-int process_epoch(char *ppath, int it, FileMap out)
-{
-    /* Open each per-process file and process it */
-
-    /* Verify headers */
-
-    /* Check whether particle data is found, stop if so (not for debugging) */
-
-    /* TODO */
-    return 0;
-}
-
-int read_particles(long long int num, char *indir, char *outdir)
-{
-    DIR *in;
-    struct dirent *dp;
-    char ppath[PATH_MAX];
-    EpochList epochs;
-    EpochList::iterator it;
-    FileMap out;
-
-    printf("Reading particles from %s.\n", indir);
-    printf("Storing trajectories in %s.\n", outdir);
-
-    /* Open particle directory and sort epoch directories */
-    if (!snprintf(ppath, PATH_MAX, "%s/particle", indir)) {
-        perror("Error: snprintf failed");
-        usage(1);
-    }
-
-    if ((in = opendir(ppath)) == NULL) {
-        perror("Error: cannot open input directory");
-        usage(1);
-    }
-
-    while (dp = readdir(in)) {
-        int epoch;
-        char *end;
-
-        if (dp->d_type != DT_DIR)
-            continue;
-
-        if (!strcmp(dp->d_name, ".") ||
-            !strcmp(dp->d_name, "..") ||
-            dp->d_name[0] != 'T')
-            continue;
-
-        epoch = strtoll(dp->d_name+2, &end, 10);
-        if (*end) {
-            perror("Error: strtoll failed");
-            closedir(in);
-            usage(1);
-        }
-
-        //printf("Found subdir %s, epoch %d.\n", dp->d_name, epoch);
-
-        epochs.push_back(epoch);
-    }
-
-    closedir(in);
-    epochs.sort();
-
-    /* Iterate through epoch frames */
-    if (generate_files(outdir, num, &out)) {
-        fprintf(stderr, "Error: particle trajectory file creation failed\n");
-        return 1;
-    }
-
-    for (it = epochs.begin(); it != epochs.end(); ++it) {
-        printf("Processing epoch %d.\n", *it);
-        if (process_epoch(ppath, *it, out)) {
-            fprintf(stderr, "Error: epoch data processing failed\n");
-            close_files(&out);
-            return 1;
-        }
-    }
-
-    close_files(&out);
-    return 0;
-}
-
 int main(int argc, char **argv)
 {
-    int ret, c, d = 0;
+    int ret, c;
     long long int num = 1;
     char indir[PATH_MAX], outdir[PATH_MAX];
 
     argv0 = argv[0];
     indir[0] = outdir[0] = '\0';
 
-    while ((c = getopt(argc, argv, "dhi:s:v:n:o:p:")) != -1) {
+    while ((c = getopt(argc, argv, "hi:s:v:n:o:p:")) != -1) {
         switch(c) {
-        case 'd': /* run in deltafs mode */
-            d = 1;
-            break;
         case 'h': /* print help */
             usage(0);
         case 'i': /* input directory (vpic output) */
@@ -287,19 +199,10 @@ int main(int argc, char **argv)
         usage(1);
     }
 
-    /* retrieve particles */
-
-    if (d) {
-        ch_inst = ch_placement_initialize(ch_type, ch_size, ch_vf, ch_seed);
-        if (!ch_inst) {
-            fprintf(stderr, "cannot init ch-placement");
-            exit(1);
-        }
-
-        ret = deltafs_read_particles(num, indir, outdir);
-    } else {
-        ret = read_particles(num, indir, outdir);
+    if (!(ch_inst = ch_placement_initialize(ch_type, ch_size, ch_vf, ch_seed))) {
+        fprintf(stderr, "cannot init ch-placement");
+        exit(1);
     }
 
-    return ret;
+    return deltafs_read_particles(num, indir, outdir);
 }
