a49a247ce3502eb9a0cd2dbaed2c2de2c99d29ef
Qing Zheng <zhengqmark@gmail.com>
2017-10-31 01:50:58 +0000

    fix mpirun cmd options

--- tests/preload-test.sh
+++ tests/preload-test.sh
@@ -50,7 +50,7 @@ if [ x"$MPI" = xmpich ]; then
     RC=$?
 
 elif [ x"$MPI" = xopenmpi ]; then
-    mpirun.openmpi -np $MPI_PROCS -tag-output -bind-to core \
+    mpirun.openmpi -np $MPI_PROCS -tag-output -bind-to-core \
         -x "LD_PRELOAD=$BUILD_PREFIX/src/libdeltafs-preload.so" \
         -x "PRELOAD_Bypass_deltafs=1" \
         -x "PRELOAD_Bypass_shuffle=0" \
