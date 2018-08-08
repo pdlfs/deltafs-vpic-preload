22ca820e76d3c6cdc3928f8069c2ee9c40652d4a
Qing Zheng <zhengq@cs.cmu.edu>
2018-08-08 03:12:58 -0400

    add util functions for converting hex strings to/from filenames

--- src/common.h
+++ src/common.h
@@ -60,6 +60,10 @@ uint64_t now_micros_coarse();
 /* convert posix timeval to micros */
 uint64_t timeval_to_micros(const struct timeval* tv);
 
+/* convert file names to hex strings and vice versa */
+void fn2hex(std::string* input);
+void hex2fn(std::string* input);
+
 /* log message into a given file using unbuffered io. */
 void LOG(int fd, int e, const char* fmt, ...);
 
