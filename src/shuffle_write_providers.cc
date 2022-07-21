e06722f7c25dbeaf29fd67ed13bf9b0dc34f95d5
Chuck Cranor <chuck@ece.cmu.edu>
2022-07-21 16:34:23 -0400

    remove dead code

--- src/shuffle_write_providers.cc
+++ src/shuffle_write_providers.cc
@@ -81,64 +81,6 @@ void shuffle_write_range_debug(shuffle_ctx_t* ctx, char* buf,
 }
 }  // namespace
 
-#if 0
-int shuffle_write_nohash(shuffle_ctx_t* ctx, const char* fname,
-                         unsigned char fname_len, char* data,
-                         unsigned char data_len, int epoch) {
-#define SHUFFLE_BUF_LEN 255
-
-  char buf[SHUFFLE_BUF_LEN];
-
-  float prop = ::get_indexable_property(data, data_len);
-
-  int peer_rank = int(prop);
-  int rank = shuffle_rank(ctx);
-  int rv;
-  // fprintf(stderr, "shuffle_write_nohash: %f %d\n", prop, peer_rank);
-
-  range_ctx_t* rctx = &pctx.rctx;
-
-  // fprintf(stderr, "fname comp: %d %d\n", ctx->fname_len, fname_len);
-  // fprintf(stderr, "data comp: %d %d\n", ctx->data_len, data_len);
-
-  assert(ctx == &pctx.sctx);
-  assert(ctx->extra_data_len + ctx->data_len < 255 - ctx->fname_len - 1);
-  if (ctx->fname_len != fname_len) ABORT("bad filename len");
-  if (ctx->data_len != data_len) ABORT("bad data len");
-
-  unsigned char base_sz = 1 + fname_len + data_len;
-  unsigned char buf_sz = base_sz + ctx->extra_data_len;
-
-  buf_sz = msgfmt_write_data(buf, SHUFFLE_BUF_LEN, fname, fname_len, data,
-                             data_len, ctx->extra_data_len);
-
-  /* bypass rpc if target is local */
-  if (peer_rank == rank && !ctx->force_rpc) {
-    rv = native_write(fname, fname_len, data, data_len, epoch);
-    return rv;
-  }
-
-  if (peer_rank == -1 || peer_rank >= pctx.comm_sz) {
-    return 0;
-  }
-
-  rctx->ts_writes_shuffled++;
-
-  // fprintf(stderr, "At SRC: %d, SEND to %d, P: %02x%02x%02x\n", rank,
-  // peer_rank, buf[0], buf[1], buf[2]);
-
-  if (ctx->type == SHUFFLE_XN) {
-    /* TODO: Also handle flushed OOB packets */
-    int padded_sz = buf_sz;
-    xn_shuffle_enqueue(static_cast<xn_ctx_t*>(ctx->rep), buf, buf_sz, epoch,
-                       peer_rank, rank);
-  } else {
-    nn_shuffler_enqueue(buf, buf_sz, epoch, peer_rank, rank);
-  }
-  return 0;
-}
-#endif
-
 timespec diff(timespec start, timespec end) {
   timespec temp;
   if ((end.tv_nsec - start.tv_nsec) < 0) {
