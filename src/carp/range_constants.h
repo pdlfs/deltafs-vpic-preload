46fe27e252d6b9bdf29abd3e1403b819df629e0b
Chuck Cranor <chuck@ece.cmu.edu>
2023-05-02 08:55:26 -0400

    remove commented out section

--- src/carp/range_constants.h
+++ src/carp/range_constants.h
@@ -14,12 +14,3 @@
 #define CARP_MAXPARTSZ       256     /* max particle sz (key+filename+data) */
 #define CARP_RENEG_INT       500000
 #define CARP_FLOATCOMP_THOLD 1e-3
-
-#if 0
-#define DEFAULT_PVTCNT 256
-#define DEFAULT_OOBSZ 512
-namespace pdlfs {
-const size_t kRenegInterval = 500000;
-const char* kRenegPolicyIntraEpoch = "InvocationIntraEpoch";
-}  // namespace pdlfs
-#endif
